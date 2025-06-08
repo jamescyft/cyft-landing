bb027ca8a095c89c8ebe53782acd8a637d751ce6
James <james@cyft.ai>
2025-06-08 16:18:06 -0400

    chore: Remove test file

--- test-env.js
+++
@@ -1,6 +0,0 @@
-import { ENV } from './src/config/environment.js';
-
-console.log('Google Sheets Config:');
-console.log('- Enabled:', ENV.googleSheets.enabled);
-console.log('- Script URL:', ENV.googleSheets.scriptUrl);
-console.log('- Full ENV:', JSON.stringify(ENV, null, 2)); 
